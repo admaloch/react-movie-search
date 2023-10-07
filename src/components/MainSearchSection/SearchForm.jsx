b725551ca3235d953d2d42e8b3a7b656e81f41af
Andrew Maloch <admaloch91@gmail.com>
2023-10-07 14:45:45 -0400

    refactor attempt 3

--- src/components/MainSearchSection/SearchForm.jsx
+++ src/components/MainSearchSection/SearchForm.jsx
@@ -2,31 +2,29 @@
 import SearchList from "./SearchList";
 import { useTheme, useThemeUpdate } from "../../store/APIContext";
 
-import axios from 'axios';
+
 import { useEffect } from "react";
-const BASE_URL = 'https://omdbapi.com/?s='
-const api_key = '&apikey=84200d7a'
+
 
 const SearchForm = ({ types }) => {
-    const contextObj = useTheme()
-    const searchTerm = contextObj.search
-    const [searchRes, setSearchRes] = contextObj.result
-    const handleChange = useThemeUpdate()
+
+    const searchTerm = useTheme().search
+    const [searchRes, setSearchRes] = useTheme().result
+    const updateThemeObj = useThemeUpdate()
+    const handleChange = useThemeUpdate().inputHandler
+    const fetchMovie = useThemeUpdate().apiHandler
+
     const currType = types.filter(item => item.isActive === true)[0]
+    const searchParam = currType.apiParam
 
-        const searchParam = currType.apiParam
-        // const searchParam = '&type=movie'
 
-        useEffect(
-            function fetchMovieInfoOnChange() {
-                async function fetchMovie() {
-                    const searchResults = await axios.get(`${BASE_URL}${searchTerm}&page=1${api_key}${searchParam}`)
-                    setSearchRes(searchResults.data)
-                }
-                fetchMovie();
-            },
-            [searchTerm]
-        )
+
+    useEffect(
+        function fetchMovieInfoOnChange() {
+            fetchMovie(searchParam);
+        },
+        [searchTerm]
+    )
 
 
     return (
--- src/store/APIContext.jsx
+++ src/store/APIContext.jsx
@@ -3,7 +3,9 @@ import React, { useContext, useState } from "react"
 
 const ThemeContext = React.createContext()
 const ThemeUpdateContext = React.createContext()
-
+import axios from 'axios';
+const BASE_URL = 'https://omdbapi.com/?s='
+const api_key = '&apikey=84200d7a'
 
 
 export function useTheme() {
@@ -21,13 +23,17 @@ export function ThemeProvider({ children }) {
         setSearchTerm(e.target.value)
 
     }
+
+    async function fetchMovie(searchParam) {
+        const searchResults = await axios.get(`${BASE_URL}${searchTerm}&page=1${api_key}${searchParam}`)
+        setSearchRes(searchResults.data)
+    }
    
-
-
+    
 
     return (
         <ThemeContext.Provider value={{search: searchTerm, result: [searchRes, setSearchRes]}}>
-            <ThemeUpdateContext.Provider value={handleChange}>
+            <ThemeUpdateContext.Provider value={{inputHandler: handleChange, apiHandler: fetchMovie}}>
                 {children}
             </ThemeUpdateContext.Provider>
         </ThemeContext.Provider>
