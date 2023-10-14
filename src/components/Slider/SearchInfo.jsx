c04376644f257201cc6feed49cecfca97a84e8cf
Andrew Maloch <admaloch91@gmail.com>
2023-10-14 19:13:03 -0400

    Setting up progress bar state

--- src/components/Slider/SearchInfo.jsx
+++ src/components/Slider/SearchInfo.jsx
@@ -1,19 +1,30 @@
 import "./SearchInfo.css"
 import { randomColorGen } from '../utility/utility.js'
 import { useTheme } from "../../store/APIContext"
+import { useEffect } from "react"
+import { useState } from "react"
 
 const SearchInfo = ({ types, sliderIndex }) => {
+
+    const [numProgBar, setNumProgBar] = useState(0)
     const { submittedSearch, searchRes } = useTheme()
     const currType = types.filter(item => item.isActive === true)[0]
     let lightOrDarkText = currType.type === 'Movie' ? 'light' : 'dark'
     const spanColor = { color: randomColorGen(lightOrDarkText) }
     const currSearch = submittedSearch.charAt(0).toUpperCase() + submittedSearch.slice(1)
- 
-    console.log(submittedSearch)
 
-    // const itemsPerScreen = parseInt(getComputedStyle(document.querySelector('.slider')).getPropertyValue('--images-per-screen'))
+    
 
-    // const numItems = sear
+
+    useEffect(
+        function progressBarFunc() {
+            const itemsPerScreen = parseInt(getComputedStyle(document.querySelector('.slider')).getPropertyValue('--images-per-screen'))
+            const numItems = searchRes.length
+            setNumProgBar(numItems / itemsPerScreen)
+        },
+        [submittedSearch]
+    );
+    console.log(numProgBar)
 
     return (
 
