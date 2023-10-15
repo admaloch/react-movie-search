6cc1f63f0bce167e8760185d8af5d3ae04d40f5d
Andrew Maloch <admaloch91@gmail.com>
2023-10-15 17:03:25 -0400

    use reducer done

--- src/components/Slider/MainSlider.jsx
+++ src/components/Slider/MainSlider.jsx
@@ -1,36 +1,51 @@
 
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
 import { useState } from 'react';
 import { useTheme } from '../../store/APIContext';
 
 import SearchInfo from './search-info/SearchInfo';
 import SliderContainer from './slider-container/SliderContainer';
 
+const reducer = (sliderIndex, action) => {
+    let progBar = action.progBar
+    let newIndexVal = 0
+    switch (action.type) {
+        case 'increment':
+            sliderIndex.index + 1 > progBar.length - 1
+                ? newIndexVal = 0
+                : newIndexVal = sliderIndex.index + 1
+    }
+    switch (action.type) {
+        case 'decrement':
+            sliderIndex.index - 1 === - 1
+                ? newIndexVal = progBar.length - 1
+                : newIndexVal = sliderIndex.index - 1
+    }
+            document.querySelector('.slider').style.setProperty('--slider-index', newIndexVal);
+            return { index: newIndexVal }
+}
+
 const MainSlider = ({ types }) => {
     const { searchRes, submittedSearch } = useTheme()
-    const [sliderIndex, setSliderIndex] = useState(0)
+    const [sliderIndex, dispatch] = useReducer(reducer, { index: 0 })
     const [progBar, setProgBar] = useState([])
 
-    const decreaseIndexHandler = () => {
-        let newIndexVal = 0
-        sliderIndex - 1 === -1
-            ? newIndexVal = progBar.length - 1
-            : newIndexVal = sliderIndex - 1
-        document.querySelector('.slider').style.setProperty('--slider-index', newIndexVal);
-        setSliderIndex(newIndexVal)
+    const increaseIndexHandler = () => {
+        dispatch({ type: 'increment', progBar: progBar })
+
     }
 
-    const increaseIndexHandler = () => {
-        let newIndexVal = 0
-        sliderIndex + 1 > progBar.length - 1
-            ? newIndexVal === 0
-            : newIndexVal = sliderIndex + 1
-        setSliderIndex(newIndexVal)
-        document.querySelector('.slider').style.setProperty('--slider-index', newIndexVal);
+    const decreaseIndexHandler = () => {
+        dispatch({ type: 'decrement', progBar: progBar })
+
     }
+
+
 
     useEffect(
+
         function progressBarFunc() {
+
             const itemsPerScreen = parseInt(getComputedStyle(document.querySelector('.slider')).getPropertyValue('--images-per-screen'))
             const numItems = searchRes.Search && searchRes.Search.length
             const numOfBlocks = Math.ceil(numItems / itemsPerScreen)
@@ -38,7 +53,7 @@ const MainSlider = ({ types }) => {
             for (let i = 0; i < numOfBlocks; i++) {
                 blockArr.push({ id: i, isActive: false })
             }
-            blockArr[sliderIndex] = { id: sliderIndex, isActive: true }
+            blockArr[sliderIndex.index] = { id: sliderIndex.index, isActive: true }
             setProgBar(blockArr)
         },
         [submittedSearch, sliderIndex]
@@ -46,7 +61,7 @@ const MainSlider = ({ types }) => {
 
     return (
         <>
-            <Search/Info
+            <SearchInfo
                 progBar={progBar}
                 types={types}
             />
