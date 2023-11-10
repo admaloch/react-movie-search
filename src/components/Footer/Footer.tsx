76191690313fb19ae41712feb53af4330d8840ac
Andrew Maloch <admaloch91@gmail.com>
2023-11-10 11:21:35 -0500

    ts setup complete

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,16 +2,14 @@
 import './Footer.css'
 import GitHubIcon from '@mui/icons-material/GitHub';
 
-export default function Footer() {
+export default function Footer(): JSX.Element {
     return (
         <div className='footer'>
 
             <p>Developed by Davis Maloch</p>
             <p>Created using OMDB API</p>
             <a href="https://github.com/admaloch">
-                <GitHubIcon                     sx={{
-                  marginRight: .5
-                    }} />
+                <GitHubIcon sx={{ marginRight: .5 }}/>
                 Github</a>
         </div>
     )
