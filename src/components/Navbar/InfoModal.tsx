043632a2d75d3b56c5082432cc7536d8276f1e2d
Andrew Maloch <admaloch91@gmail.com>
2023-11-25 15:19:50 -0500

    prepared for github

--- src/components/Navbar/InfoModal.tsx
+++ src/components/Navbar/InfoModal.tsx
@@ -9,12 +9,10 @@ export default function InfoModal({ open, closeModal }:ModalProps): JSX.Element 
             open={open}
         >
             <div className="nav-modal-container">
-                <h3>Instructions:</h3>
-                <p>Filter the search results by selecting between the menu items</p>
-                <p>Search for </p>
                 <h4>Information:</h4>
                 <p>This was developed by Davis Maloch and is powered by OMDB API.</p>
                 <p>For questions or concerns email me @ admaloch91@gmail.com </p>
+                
             </div>
         </Modal>
     )
