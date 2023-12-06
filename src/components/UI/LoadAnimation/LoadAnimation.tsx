37df1442eea995594c3b9e01cdc58053be8cd292
Andrew Maloch <admaloch91@gmail.com>
2023-12-06 07:46:28 -0500

    design tweaks

--- src/components/UI/LoadAnimation/LoadAnimation.tsx
+++ src/components/UI/LoadAnimation/LoadAnimation.tsx
@@ -2,10 +2,10 @@ import LoadingProps from '../../../models/LoadingProps'
 import './LoadAnimation.css'
 import React from 'react'
 
-export default function LoadAnimation({ isLoading }: LoadingProps):JSX.Element {
+export default function LoadAnimation({ isLoading }: LoadingProps): JSX.Element {
     return (
         <>
-            <div style={{ opacity: isLoading ? 1 : 0 }} className="load-animation"><div></div><div></div><div></div><div></div></div>
+            <div style={{ display: isLoading ? 'block' : 'none' }} className="load-animation"><div></div><div></div><div></div><div></div></div>
         </>
     )
 }
