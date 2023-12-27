5b31f50137468d1498a6f0260f7702be03e3346f
Andrew Maloch <admaloch91@gmail.com>
2023-12-27 08:04:26 -0500

    last minute design tweaks

--- src/components/Navbar/InfoModal.tsx
+++ src/components/Navbar/InfoModal.tsx
@@ -1,7 +1,13 @@
 import Modal from '../UI/Modal'
 import { ModalProps } from '../../models/ModalProps'
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { IconButton } from '@mui/material';
+
 
 export default function InfoModal({ open, closeModal }: ModalProps): JSX.Element {
+
+
+
     return (
         <Modal
             closeModal={closeModal}
@@ -11,9 +17,17 @@ export default function InfoModal({ open, closeModal }: ModalProps): JSX.Element
                 <h4>Information:</h4>
                 <p>This was developed by Davis Maloch with a special thanks to OMDB API.</p>
                 <p>For questions or recommended changes, email me at admaloch91@gmail.com </p>
-                <p>Please ceckout my personal github <a href="https://github.com/admaloch"></a>, or work github <a href="https://github.com/admaloch-dos"></a>to see more of my recent projects. </p>
+                <p>Please checkout my personal Github<a target="_blank" href="https://github.com/admaloch">
+                    <IconButton style={{ padding: '0 .1rem' }} className="custom-icon">
+                        <GitHubIcon />
+                    </IconButton></a>,
+                    or work Github<a target="_blank" href="https://github.com/admaloch-dos">
+                        <IconButton style={{ padding: '0 .4rem 0 .1rem' }} className="custom-icon">
+                            <GitHubIcon />
+                        </IconButton></a>
+                    to see more of my recent projects. </p>
 
             </div>
-        </Modal>
+        </Modal >
     )
 }
