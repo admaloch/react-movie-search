322365feaf43907b8bf23fd21c11d68bd132964b
Andrew Maloch <admaloch91@gmail.com>
2023-12-28 06:50:22 -0500

    another attempted change to package.json

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-movie-search/',
-})
+  base: '/react-movie-search/', // Match the value with the homepage field in package.json
+});
