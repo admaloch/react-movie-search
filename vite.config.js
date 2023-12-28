d51dcf7ab19e08749e3b89ce6717113f0b9048c5
Andrew Maloch <admaloch91@gmail.com>
2023-12-28 06:56:47 -0500

    changes to vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-movie-search/', // Match the value with the homepage field in package.json
+  base: '/react-movie-search/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', // Add this line
+  },
 });
