7a6e9e6f44542e160db2c124236ff607093c5f45
Andrew Maloch <admaloch91@gmail.com>
2024-07-22 10:00:41 -0400

    finishing nav

--- client/src/components/HomePage/HomeLayout.tsx
+++ client/src/components/HomePage/HomeLayout.tsx
@@ -4,11 +4,12 @@ import './HomeLayout.css'
 import { Outlet } from 'react-router-dom'
 
 import bgBrain from '../../../public/big-brain.png'
+import HomePageNavAlt from './HomePageNav'
 
 export default function HomeLayout() {
   return (
     <BGSection bgClass="theatre-bg4">
-      <HomePageNav />
+      <HomePageNavAlt />
       <main className='main-content-container'>
         <Outlet />
         <img className='background-img' src={bgBrain} alt="background-brain" />
--- client/src/components/HomePage/HomePageNav.tsx
+++ client/src/components/HomePage/HomePageNav.tsx
@@ -1,25 +1,46 @@
-import { NavLink } from 'react-router-dom'
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import './HomeLayout.css'
+import { NavLink } from "react-router-dom";
 import brain from '../../../public/brain.png'
 
-export default function HomePageNav() {
+function HomePageNavAlt() {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle(
+            "responsive_nav"
+        );
+    };
+
     return (
-        <header className="home-page-nav">
-            <div className="nav-container">
-                <div className="logo">
+        <header>
+            <NavLink className="logo" to="/">
+                
                     <h3>MovieBrain</h3>
                     <img src={brain} alt="brain-icon" />
-                </div>
-                <nav className="nav-links">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/search">Search</NavLink>
-                    <NavLink to="/register">Register</NavLink>
-                    <NavLink to="/login">Login</NavLink>
+               
+            </NavLink>
 
 
-                </nav>
-            </div>
-
-
+            <nav ref={navRef}>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/search">Search</NavLink>
+                <NavLink to="/register">Register</NavLink>
+                <NavLink to="/login">Login</NavLink>
+                <button
+                    className="nav-btn nav-close-btn"
+                    onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button
+                className="nav-btn"
+                onClick={showNavbar}>
+                <FaBars />
+            </button>
         </header>
-    )
+    );
 }
+
+export default HomePageNavAlt;
