7fafc572d5cc361dde0764d0f7fc84be3a4bc013
Andrew Maloch <admaloch91@gmail.com>
2024-08-06 08:17:35 -0400

    finished more redux

---
+++ backend/seeds/index.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const User = require('../models/User'); // Adjust the path as necessary
+const {userModelData} = require('./userData')
+const bcrypt = require('bcrypt')
+
+const dbURI = 'mongodb+srv://admaloch:Kik0d0ggie246810@cluster0.5ji8t1s.mongodb.net/MoviesDB?retryWrites=true&w=majority&appName=Cluster0'
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbURI)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+
+const seedDB = async () => {
+    try {
+        await connectDB(); // Connect to the database
+
+        for (let userItem of userModelData) {
+            const hashedPwd = await bcrypt.hash(userItem.password, 10) // salt rounds
+
+            const user = new User({
+                username: userItem.username,
+                email: userItem.email,
+                password: hashedPwd,
+                likedMovies: userItem.likedMovies,
+                reviews: []
+            });
+            await user.save();
+        }
+        console.log('Data seeded successfully');
+    } catch (err) {
+        console.error('Error seeding data:', err);
+    } finally {
+        mongoose.connection.close(); // Ensure connection is closed
+    }
+};
+
+// Run the script
+seedDB();
---
+++ backend/seeds/userData.js
@@ -0,0 +1,309 @@
+// Define user data
+module.exports.userModelData = [
+    {
+        username: "RetroDreamer",
+        email: "retro.dreamer@vintage.com",
+        password: "Retro$2024",
+        likedMovies: [
+            { imdbId: "tt0076765", hasWatched: false },
+            { imdbId: "tt0067541", hasWatched: true },
+            { imdbId: "tt0060827", hasWatched: false },
+            { imdbId: "tt0084787", hasWatched: true },
+            { imdbId: "tt0063350", hasWatched: false },
+            { imdbId: "tt0070040", hasWatched: true },
+            { imdbId: "tt0074486", hasWatched: false },
+            { imdbId: "tt0065214", hasWatched: true },
+            { imdbId: "tt0074102", hasWatched: false },
+            { imdbId: "tt0070510", hasWatched: true }
+        ]
+    },
+    {
+        username: "CinephileVoyager",
+        email: "voyager.cinephile@filmfest.com",
+        password: "CinePhile#2024",
+        likedMovies: [
+            { imdbId: "tt0062622", hasWatched: false },
+            { imdbId: "tt0057565", hasWatched: true },
+            { imdbId: "tt0055630", hasWatched: false },
+            { imdbId: "tt0077158", hasWatched: true },
+            { imdbId: "tt0071396", hasWatched: false },
+            { imdbId: "tt0070083", hasWatched: true },
+            { imdbId: "tt0067023", hasWatched: false },
+            { imdbId: "tt0087800", hasWatched: true },
+            { imdbId: "tt0077402", hasWatched: false },
+            { imdbId: "tt0075148", hasWatched: true }
+        ]
+    },
+    {
+        username: "AvantGardeGuru",
+        email: "guru.avantgarde@cinema.com",
+        password: "AvantGarde!2024",
+        likedMovies: [
+            { imdbId: "tt0059646", hasWatched: false },
+            { imdbId: "tt0080025", hasWatched: true },
+            { imdbId: "tt0062512", hasWatched: false },
+            { imdbId: "tt0070355", hasWatched: true },
+            { imdbId: "tt0067992", hasWatched: false },
+            { imdbId: "tt0077711", hasWatched: true },
+            { imdbId: "tt0056801", hasWatched: false },
+            { imdbId: "tt0076786", hasWatched: true },
+            { imdbId: "tt0073312", hasWatched: false },
+            { imdbId: "tt0056808", hasWatched: true }
+        ]
+    },
+    {
+        username: "CelluloidSculptor",
+        email: "sculptor.celluloid@frames.com",
+        password: "Celluloid$2024",
+        likedMovies: [
+            { imdbId: "tt0066265", hasWatched: false },
+            { imdbId: "tt0066585", hasWatched: true },
+            { imdbId: "tt0082694", hasWatched: false },
+            { imdbId: "tt0065214", hasWatched: true },
+            { imdbId: "tt0079947", hasWatched: false },
+            { imdbId: "tt0083922", hasWatched: true },
+            { imdbId: "tt0071360", hasWatched: false },
+            { imdbId: "tt0062711", hasWatched: true },
+            { imdbId: "tt0079817", hasWatched: false },
+            { imdbId: "tt0082406", hasWatched: true }
+        ]
+    },
+    {
+        username: "FilmNostalgist",
+        email: "nostalgist.film@nostalgia.com",
+        password: "FilmNostalgia#2024",
+        likedMovies: [
+            { imdbId: "tt0077766", hasWatched: false },
+            { imdbId: "tt0060897", hasWatched: true },
+            { imdbId: "tt0064791", hasWatched: false },
+            { imdbId: "tt0082198", hasWatched: true },
+            { imdbId: "tt0070047", hasWatched: false },
+            { imdbId: "tt0067116", hasWatched: true },
+            { imdbId: "tt0077405", hasWatched: false },
+            { imdbId: "tt0070871", hasWatched: true },
+            { imdbId: "tt0083922", hasWatched: false },
+            { imdbId: "tt0057012", hasWatched: true }
+        ]
+    },
+    {
+        username: "ClassicFilmFanatic",
+        email: "fanatic.classicfilm@classic.com",
+        password: "ClassicFilm*2024",
+        likedMovies: [
+            { imdbId: "tt0069204", hasWatched: false },
+            { imdbId: "tt0074486", hasWatched: true },
+            { imdbId: "tt0073629", hasWatched: false },
+            { imdbId: "tt0076945", hasWatched: true },
+            { imdbId: "tt0056808", hasWatched: false },
+            { imdbId: "tt0070360", hasWatched: true },
+            { imdbId: "tt0057128", hasWatched: false },
+            { imdbId: "tt0073766", hasWatched: true },
+            { imdbId: "tt0081398", hasWatched: false },
+            { imdbId: "tt0065214", hasWatched: true }
+        ]
+    },
+    {
+        username: "ArthouseAdventurer",
+        email: "adventurer.arthouse@cinematic.com",
+        password: "ArthouseAdventure2024",
+        likedMovies: [
+            { imdbId: "tt0056058", hasWatched: false },
+            { imdbId: "tt0054387", hasWatched: true },
+            { imdbId: "tt0076735", hasWatched: false },
+            { imdbId: "tt0073092", hasWatched: true },
+            { imdbId: "tt0068390", hasWatched: false },
+            { imdbId: "tt0070510", hasWatched: true },
+            { imdbId: "tt0073822", hasWatched: false },
+            { imdbId: "tt0083658", hasWatched: true },
+            { imdbId: "tt0066808", hasWatched: false },
+            { imdbId: "tt0072500", hasWatched: true }
+        ]
+    },
+    {
+        username: "VintageCinephile",
+        email: "cinephile.vintage@oldschool.com",
+        password: "VintageCinephile2024",
+        likedMovies: [
+            { imdbId: "tt0060862", hasWatched: false },
+            { imdbId: "tt0062323", hasWatched: true },
+            { imdbId: "tt0061735", hasWatched: false },
+            { imdbId: "tt0070034", hasWatched: true },
+            { imdbId: "tt0070897", hasWatched: false },
+            { imdbId: "tt0070743", hasWatched: true },
+            { imdbId: "tt0065214", hasWatched: false },
+            { imdbId: "tt0080569", hasWatched: true },
+            { imdbId: "tt0077975", hasWatched: false },
+            { imdbId: "tt0070057", hasWatched: true }
+        ]
+    },
+    {
+        username: "CinematicHistorian",
+        email: "historian.cinematic@archive.com",
+        password: "CinematicHist2024",
+        likedMovies: [
+            { imdbId: "tt0072443", hasWatched: false },
+            { imdbId: "tt0053125", hasWatched: true },
+            { imdbId: "tt0062312", hasWatched: false },
+            { imdbId: "tt0061578", hasWatched: true },
+            { imdbId: "tt0073713", hasWatched: false },
+            { imdbId: "tt0071994", hasWatched: true },
+            { imdbId: "tt0065214", hasWatched: false },
+            { imdbId: "tt0072438", hasWatched: true },
+            { imdbId: "tt0078971", hasWatched: false },
+            { imdbId: "tt0082198", hasWatched: true }
+        ]
+    },
+    {
+        username: "CinemaNostalgic",
+        email: "nostalgic.cinema@retro.com",
+        password: "CinemaNostalgia2024",
+        likedMovies: [
+            { imdbId: "tt0052357", hasWatched: false },
+            { imdbId: "tt0060107", hasWatched: true },
+            { imdbId: "tt0063350", hasWatched: false },
+            { imdbId: "tt0087884", hasWatched: true },
+            { imdbId: "tt0077711", hasWatched: false },
+            { imdbId: "tt0082971", hasWatched: true },
+            { imdbId: "tt0065214", hasWatched: false },
+            { imdbId: "tt0060827", hasWatched: true },
+            { imdbId: "tt0079947", hasWatched: false },
+            { imdbId: "tt0082694", hasWatched: true }
+        ]
+    },
+    {
+        username: "ArthouseAfficionado",
+        email: "afficionado.arthouse@cine.com",
+        password: "ArthouseFan2024",
+        likedMovies: [
+            { imdbId: "tt0070040", hasWatched: false },
+            { imdbId: "tt0050083", hasWatched: true },
+            { imdbId: "tt0053125", hasWatched: false },
+            { imdbId: "tt0088846", hasWatched: true },
+            { imdbId: "tt0061184", hasWatched: false },
+            { imdbId: "tt0062622", hasWatched: true },
+            { imdbId: "tt0078767", hasWatched: false },
+            { imdbId: "tt0073195", hasWatched: true },
+            { imdbId: "tt0066026", hasWatched: false },
+            { imdbId: "tt0087262", hasWatched: true }
+        ]
+    },
+    {
+        username: "RetroVisionary",
+        email: "visionary.retro@cinema.com",
+        password: "RetroVision2024",
+        likedMovies: [
+            { imdbId: "tt0075860", hasWatched: false },
+            { imdbId: "tt0053125", hasWatched: true },
+            { imdbId: "tt0061184", hasWatched: false },
+            { imdbId: "tt0079947", hasWatched: true },
+            { imdbId: "tt0069281", hasWatched: false },
+            { imdbId: "tt0070379", hasWatched: true },
+            { imdbId: "tt0082971", hasWatched: false },
+            { imdbId: "tt0079947", hasWatched: true },
+            { imdbId: "tt0075314", hasWatched: false },
+            { imdbId: "tt0081505", hasWatched: true }
+        ]
+    },
+    {
+        username: "CelluloidCurator",
+        email: "curator.celluloid@films.com",
+        password: "CelluloidCurate2024",
+        likedMovies: [
+            { imdbId: "tt0087800", hasWatched: false },
+            { imdbId: "tt0066011", hasWatched: true },
+            { imdbId: "tt0081398", hasWatched: false },
+            { imdbId: "tt0075148", hasWatched: true },
+            { imdbId: "tt0076807", hasWatched: false },
+            { imdbId: "tt0073195", hasWatched: true },
+            { imdbId: "tt0077416", hasWatched: false },
+            { imdbId: "tt0066921", hasWatched: true },
+            { imdbId: "tt0077869", hasWatched: false },
+            { imdbId: "tt0073195", hasWatched: true }
+        ]
+    },
+    {
+        username: "CinephileCollector",
+        email: "collector.cinephile@films.com",
+        password: "CinephileCollect2024",
+        likedMovies: [
+            { imdbId: "tt0057128", hasWatched: false },
+            { imdbId: "tt0081505", hasWatched: true },
+            { imdbId: "tt0073195", hasWatched: false },
+            { imdbId: "tt0079947", hasWatched: true },
+            { imdbId: "tt0067116", hasWatched: false },
+            { imdbId: "tt0082934", hasWatched: true },
+            { imdbId: "tt0069281", hasWatched: false },
+            { imdbId: "tt0087262", hasWatched: true },
+            { imdbId: "tt0070379", hasWatched: false },
+            { imdbId: "tt0075860", hasWatched: true }
+        ]
+    },
+    {
+        username: "ClassicFilmBuff",
+        email: "buff.classicfilm@vintage.com",
+        password: "ClassicBuff2024",
+        likedMovies: [
+            { imdbId: "tt0082971", hasWatched: false },
+            { imdbId: "tt0067800", hasWatched: true },
+            { imdbId: "tt0074486", hasWatched: false },
+            { imdbId: "tt0066265", hasWatched: true },
+            { imdbId: "tt0079947", hasWatched: false },
+            { imdbId: "tt0081505", hasWatched: true },
+            { imdbId: "tt0071411", hasWatched: false },
+            { imdbId: "tt0073195", hasWatched: true },
+            { imdbId: "tt0081505", hasWatched: false },
+            { imdbId: "tt0077416", hasWatched: true }
+        ]
+    },
+    {
+        username: "NostalgicCelluloid",
+        email: "celluloid.nostalgic@films.com",
+        password: "NostalgiaCelluloid2024",
+        likedMovies: [
+            { imdbId: "tt0066026", hasWatched: false },
+            { imdbId: "tt0087262", hasWatched: true },
+            { imdbId: "tt0067800", hasWatched: false },
+            { imdbId: "tt0079947", hasWatched: true },
+            { imdbId: "tt0081505", hasWatched: false },
+            { imdbId: "tt0073195", hasWatched: true },
+            { imdbId: "tt0070379", hasWatched: false },
+            { imdbId: "tt0081398", hasWatched: true },
+            { imdbId: "tt0077416", hasWatched: false },
+            { imdbId: "tt0075860", hasWatched: true }
+        ]
+    },
+    {
+        username: "VintageFilmFan",
+        email: "fan.vintagefilm@films.com",
+        password: "VintageFan2024",
+        likedMovies: [
+            { imdbId: "tt0073195", hasWatched: false },
+            { imdbId: "tt0087262", hasWatched: true },
+            { imdbId: "tt0066265", hasWatched: false },
+            { imdbId: "tt0075860", hasWatched: true },
+            { imdbId: "tt0077416", hasWatched: false },
+            { imdbId: "tt0081505", hasWatched: true },
+            { imdbId: "tt0069281", hasWatched: false },
+            { imdbId: "tt0079947", hasWatched: true },
+            { imdbId: "tt0070379", hasWatched: false },
+            { imdbId: "tt0082971", hasWatched: true }
+        ]
+    },
+    {
+        username: "RetroFilmCritic",
+        email: "critic.retrofilm@vintage.com",
+        password: "RetroCritic2024",
+        likedMovies: [
+            { imdbId: "tt0067116", hasWatched: false },
+            { imdbId: "tt0073195", hasWatched: true },
+            { imdbId: "tt0079947", hasWatched: false },
+            { imdbId: "tt0077416", hasWatched: true },
+            { imdbId: "tt0081398", hasWatched: false },
+            { imdbId: "tt0069281", hasWatched: true },
+            { imdbId: "tt0082971", hasWatched: false },
+            { imdbId: "tt0070379", hasWatched: true },
+            { imdbId: "tt0087262", hasWatched: false },
+            { imdbId: "tt0073195", hasWatched: true }
+        ]
+    },
+]
