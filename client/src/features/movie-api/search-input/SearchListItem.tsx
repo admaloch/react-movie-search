15e0e1e3e2bd1e300354b92bff733fb371f6542b
Andrew Maloch <admaloch91@gmail.com>
2024-08-26 09:56:18 -0400

    updated list items in search

--- client/src/features/movie-api/search-input/SearchListItem.tsx
+++ client/src/features/movie-api/search-input/SearchListItem.tsx
@@ -10,48 +10,30 @@ import UpdateLikedList from "../../users/update-liked-list/UpdateLikedList";
 
 const SearchListItem = ({ imdbID, Poster, Title, Year }: APIResults): React.JSX.Element => {
 
-    const [isModalOpen, setIsModalOpen] = useState(false)
-
-    const [triggerGetMovieById, { data: movieItem, isLoading, isError, error }] = useLazyGetMovieByIdQuery();
-
-    async function handleListItemClick() {
-        setIsModalOpen(true)
-        triggerGetMovieById(imdbID);
-    }
-
-    const closeItemModal = () => {
-        setIsModalOpen(false)
-    }
-
-    if (isError) {
-        closeItemModal()
-        toast.error(`Error: ${error}`);
-    }
+  
 
     return (
         <>
-            <li onClick={handleListItemClick} data-id={imdbID} className="search-list-item">
-                <div className="search-item-thumbnail">
-                    <img src={Poster !== 'N/A' ? Poster : image_not_found}></img>
-                </div>
-                <div className="search-item-info">
-                    <h3>{Title}</h3>
-                    {Year && <p>{Year}</p>}
-                </div>
-                <UpdateLikedList
-                    title={Title}
-                    imdbId={imdbID}
-                    size={25}
-                />
-            </li>
+
 
             <ListItemModal
-                item={movieItem}
-                open={isModalOpen}
-                closeModal={closeItemModal}
-                isLoading={isLoading}
-                isError={isError}
-            />
+                imdbId={imdbID}
+            >
+                <li className="search-list-item">
+                    <div className="search-item-thumbnail">
+                        <img src={Poster !== 'N/A' ? Poster : image_not_found}></img>
+                    </div>
+                    <div className="search-item-info">
+                        <h3>{Title}</h3>
+                        {Year && <p>{Year}</p>}
+                    </div>
+                    <UpdateLikedList
+                        title={Title}
+                        imdbId={imdbID}
+                        size={25}
+                    />
+                </li>
+            </ListItemModal>
         </>
     )
 }
