dc900299a88405b2d4e253aca819312e81beb5e4
Davis Maloch <admaloch91@gmail.com>
2024-09-22 17:52:16 -0400

    last min design tweaks

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,18 +1,22 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import './Footer.css'
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Footer(): JSX.Element {
     return (
         <>
-            <Outlet />
-            <footer className='footer'>
-                <p>Developed by Davis Maloch</p>
-                <p>Created using OMDB API</p>
-                <a href="https://github.com/admaloch">
-                    <GitHubIcon sx={{ marginRight: .5 }} />
-                    Github</a>
-            </footer>
+           <Outlet />
+    <footer className='footer'>
+        <p>Developed by Davis Maloch</p>
+        <p>Created using OMDB API</p>
+        <a href="https://github.com/admaloch">
+            <GitHubIcon sx={{ marginRight: .5 }} />
+            Github
+        </a>
+        <p>
+            <Link to="/credits">Credits</Link> {/* Add router link to /credits */}
+        </p>
+    </footer>
         </>
 
     )
--- client/src/components/credits/Credits.tsx
+++ client/src/components/credits/Credits.tsx
@@ -1,11 +1,19 @@
+import MoodIcon from '@mui/icons-material/Mood'; // Import the happy face icon
+
+
 import './Credits.css';
 export default function Credits() {
   return (
-    <main className='main-item-content credits-section'>
-      <h1>Credits for use of Brain icons</h1>
+    <main className='main-item-content'>
+      <section className="credits-section">
+      <MoodIcon sx={{ fontSize: 100 }} /> {/* Happy face icon */}
+
+         <h1>A special thanks to the artists who took the photos and created the icons used in this app</h1>
       <ul>
         <li><a href="https://www.flaticon.com/free-icons/brain" title="brain icons">Brain icons created by Freepik - Flaticon</a></li>
     </ul>
+      </section>
+     
     </main>
   )
 }
