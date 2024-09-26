5bd3594c2869485a6548d1f16d989373d81f2b7d
Davis Maloch <admaloch91@gmail.com>
2024-09-26 18:20:16 -0400

    fixed some design issues

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -8,8 +8,7 @@ export default function Footer(): JSX.Element {
            <Outlet />
     <footer className='footer'>
         <p>Developed by Davis Maloch</p>
-        <p>Created using OMDB API</p>
-        <a href="https://github.com/admaloch">
+        <a target='_blank' href="https://github.com/admaloch">
             <GitHubIcon sx={{ marginRight: .5 }} />
             Github
         </a>
--- client/src/components/credits/Credits.tsx
+++ client/src/components/credits/Credits.tsx
@@ -7,9 +7,9 @@ export default function Credits() {
     <main className='main-item-content'>
       <section className="credits-section">
       <MoodIcon sx={{ fontSize: 100 }} /> {/* Happy face icon */}
-
-         <h1>A special thanks to the artists who took the photos and created the icons used in this app</h1>
+         <h3>A special thanks to OMDb API and the artists who took the photos and created the icons used in this app</h3>
       <ul>
+        <li><a href="https://www.omdbapi.com/">OMDb API</a></li>
         <li><a href="https://www.flaticon.com/free-icons/brain" title="brain icons">Brain icons created by Freepik - Flaticon</a></li>
     </ul>
       </section>
