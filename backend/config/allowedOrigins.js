dc658a1ab068eb4402a0695695e536aefed66260
Davis Maloch <admaloch91@gmail.com>
2024-10-26 09:49:47 -0400

    finished railway deploy

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
-const origin = process.env.NODE_ENV === 'production' 
-    ? ['https://movie-brain.netlify.app/']
-    : ['http://localhost:5173', 'http://localhost:3500'];
+const origin =
+  process.env.NODE_ENV === "production"
+    ? [process.env.CLIENT_URL]
+    : ["http://localhost:5173", "http://localhost:3500"];
 
 const allowedOrigins = origin;
 
