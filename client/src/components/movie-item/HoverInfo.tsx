8ffa7d3cb00da82b06109cd4fb07da715b6764d9
Davis Maloch <admaloch91@gmail.com>
2025-01-09 14:20:53 -0600

    fixed search item mobile hover issue

--- client/src/components/movie-item/HoverInfo.tsx
+++ client/src/components/movie-item/HoverInfo.tsx
@@ -1,14 +1,12 @@
 import ItemContent from "./ItemContent";
 import CircleAnimation from "../UI/LoadAnimation/CircleAnimation";
 import UpdateLikedList from "../../features/users/update-liked-list/UpdateLikedList";
-import ItemError from "../UI/errors/ItemError";
 import { OmdbItemWithRTK } from "../../models/RTKQueryProps";
 
 const HoverInfo = ({
   item,
   isLoading,
   isError,
-  error,
 }: OmdbItemWithRTK): JSX.Element | null => {
   
   let content;
@@ -16,11 +14,7 @@ const HoverInfo = ({
   if (isLoading) {
     content = <CircleAnimation />;
   } else if (isError || !item) {
-    content = (
-      <ItemError
-        text={`Error: ${(error as any)?.data?.message|| "Failed to load content."}`}
-      />
-    );
+    return null;
   } else {
     content = (
       <>
